ab2bf00a611204c892ea4193c0a3c552e0c489d1
Jung Seung Yong <statename@naver.com>
2021-12-29 21:14:38 +0900

    solved 10952
    step 4 10952 문제를 풀었습니다

---
+++ solve/BAEKJOON/step 4/10952.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b;
+	
+	while (1)
+	{
+		cin >> a >> b;
+		if (a + b == 0)
+			break;
+		cout << a + b << "\n";
+		
+	}
+	
+
+}
